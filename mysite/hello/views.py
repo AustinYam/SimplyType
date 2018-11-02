9427dd03f5b5d77f358de5af6288d681d7f163d3
Austin Yam <austin.yam123@gmail.com>
2018-11-01 18:07:09 -0700

    Added Database

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 # Create your views here.
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+	model = Post
 	template_name = 'home.html'
+	context_object_name = 'all_posts_list'
 
 class AboutPageView(TemplateView):
 	template_name = 'about.html'
