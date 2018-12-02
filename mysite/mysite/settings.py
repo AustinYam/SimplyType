8f1b5b1156bff46751f0d25b316b32b377d029f5
Austin Yam <austin.yam123@gmail.com>
2018-12-01 19:49:09 -0800

    register update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello.apps.HelloConfig', #new
+    'users.apps.UsersConfig',
+     'livereload',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
